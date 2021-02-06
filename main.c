4d72325804cb974faf15d7286ae03d604d5d19ea
bibartanjha <32161986+bibartanjha@users.noreply.github.com>
2021-02-06 12:24:12 -0600

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <dirent.h> 
 #define DISP_BUF_SIZE (LV_VER_RES_MAX* LV_HOR_RES_MAX)
 
 
@@ -33,8 +34,80 @@ uint32_t custom_tick_get(void);
 
 void lv_ex_get_started_1(void);
 
+int find_roms(char *games);
+
+
+
+char games_list[500] = "";
+
+int find_roms(char *games) {
+  DIR *d;
+  struct dirent *dir;
+  d = opendir("/usr/local/share/advance/rom/");
+  if (d) {
+    int i = 0;
+    while ((dir = readdir(d)) != NULL) {
+     // printf("%s\n", dir->d_name);
+        if(strlen(dir->d_name)>4){
+            //if the file_name is longer than .zip
+            char s [50];
+            strcpy(s,dir->d_name);
+            
+            
+            //if statements for all the weird file names
+            if (!(strcmp(s, "rtype2.zip"))) {
+                strcpy(s, "R-type.zip");
+            }
+            
+            else if (!(strcmp(s, "mosaic.zip"))) {
+                strcpy(s, "Tetris.zip");
+            }
+            
+            else if (!(strcmp(s, "roadrunn.zip"))) {
+                strcpy(s, "Road Runner.zip");
+            }
+            
+            else if (!(strcmp(s, "dkong.zip"))) {
+                strcpy(s, "Donkey Kong.zip");
+            }
+            
+            else if (!(strcmp(s, "playball.zip"))) {
+                strcpy(s, "Play Ball.zip");
+            }
+            
+            else if (!(strcmp(s, "arknoid2.zip"))) {
+                strcpy(s, "Brick Breaker.zip");
+            }
+            
+            
+            //change the first letter 
+            if(s[0] >= 'a' && s[0] <= 'z') {
+             s[0] = s[0] - 32;
+            }            
+            if (i == 0){
+            s[strlen(s) - 4] = '\0';
+            strcpy(games,s);
+           // printf("%s",games);
+            i++;
+            }
+            else{
+            //s[strlen(s) - 4] = '\n';
+            s[strlen(s) - 4] = '\0';
+            i =strlen(games) + 1;
+            games[i-1] = '\n';
+            strcat(games,s);                
+            }        
+        }
+        
+    }
+    closedir(d);
+  }
+
+  return(0);
+}
+
 int main(void)
-{
+{   
     //system("sudo echo 0 | sudo tee -a /sys/class/graphics/fbcon/cursor_blink");
     int fd = open("/sys/class/graphics/fbcon/cursor_blink", O_WRONLY);
     write(fd, "0", 1);
@@ -72,12 +145,15 @@ int main(void)
 
 void lv_ex_get_started_1(void)
 {
-     LV_THEME_DEFAULT_INIT(lv_theme_get_color_primary(), lv_theme_get_color_primary(),
-        LV_THEME_MATERIAL_FLAG_DARK,
-        lv_theme_get_font_small(), lv_theme_get_font_normal(), lv_theme_get_font_subtitle(), lv_theme_get_font_title());
-     g = lv_group_create();
+    find_roms(games_list);
 
- 	evdev_init(); 
+
+    LV_THEME_DEFAULT_INIT(lv_theme_get_color_primary(), lv_theme_get_color_primary(),
+    LV_THEME_MATERIAL_FLAG_DARK,
+    lv_theme_get_font_small(), lv_theme_get_font_normal(), lv_theme_get_font_subtitle(), lv_theme_get_font_title());
+    g = lv_group_create();
+
+    evdev_init(); 
     lv_indev_drv_init(&kb_drv);
     kb_drv.type = LV_INDEV_TYPE_KEYPAD;
     kb_drv.read_cb = evdev_read;
@@ -99,7 +175,7 @@ void lv_ex_get_started_1(void)
     lv_roller_set_visible_row_count(roller, 4);
     lv_obj_set_width(roller, 128);
     lv_obj_set_height(roller, 160);
-    lv_roller_set_options(roller, "Pacman\nRobby\nGridlee\nCarpolo\nPolyplay\nMad Gear", LV_ROLLER_MODE_NORMAL);
+    lv_roller_set_options(roller, games_list, LV_ROLLER_MODE_NORMAL);
     lv_obj_set_event_cb(roller, roller_event_cb);
     lv_group_add_obj(g, roller);
 }
@@ -137,8 +213,8 @@ static void roller_event_cb(lv_obj_t* obj, lv_event_t event)
          }
         
         char filepath[100];
-        
-        strcpy(filepath, "/home/debian/advmame.sh ");
+        //strcpy(filepath, "/home/debian/advmame.sh ");
+        strcpy(filepath, "FRAMEBUFFER=/dev/fb0 advmame ");
         strcat(filepath, buf);
         
         char command[110];
@@ -149,4 +225,3 @@ static void roller_event_cb(lv_obj_t* obj, lv_event_t event)
         
     }
 }
-
